a468477fc2c1dc2d9e7be0dd65ee513aa858e6de
Hayk <Hayk.hovsepyan19@gmail.com>
2022-12-17 02:01:00 +0400

    change homework1

--- src/homework/Homework2.java
+++ src/homework/Homework2.java
@@ -4,7 +4,7 @@ public class Homework2 {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello from homework");
+        System.out.println("Hello from homework 1");
 
     }
 }

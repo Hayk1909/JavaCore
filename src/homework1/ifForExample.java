1290f08af63d72f6fd6e5a5fb77558d2fe3c7690
Hayk <Hayk.hovsepyan19@gmail.com>
2022-12-20 18:23:11 +0400

    for example2

---
+++ src/homework1/ifForExample.java
@@ -0,0 +1,20 @@
+package homework1;
+
+public class ifForExample {
+    public static void main(String[] args) {
+        for (int tox = 1; tox <= 10; tox++) {
+            for (int j = 1; j < tox; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= tox; k++) {
+                System.out.print("*");
+                for (int k1 = 1; k1 < k; k1 = k1 + k) {
+                    System.out.print("*");
+                }
+
+            }
+            System.out.println();
+        }
+    }
+}
+

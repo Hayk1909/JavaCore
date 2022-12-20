3d8e86253fc67948095f8907dd31a28864adee04
Hayk <Hayk.hovsepyan19@gmail.com>
2022-12-20 18:25:30 +0400

    if for

--- src/homework1/ifForExample.java
+++ src/homework1/ifForExample.java
@@ -4,10 +4,10 @@ public class ifForExample {
     public static void main(String[] args) {
         for (int tox = 1; tox <= 10; tox++) {
             for (int j = 1; j < tox; j++) {
-                System.out.print("*");
+                System.out.print("");
             }
             for (int k = 1; k <= tox; k++) {
-                System.out.print("");
+                System.out.print("*");
                 for (int k1 = 1; k1 < k; k1 = k1 + k) {
                     System.out.print("");
                 }
